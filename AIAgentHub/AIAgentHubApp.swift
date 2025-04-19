c340510c00da3126445e7b9ef0223568331dfe9f
peerapat atawatana <broccolizz@gmail.com>
2025-04-19 17:24:19 +0700

    Initial Commit

---
+++ AIAgentHub/AIAgentHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIAgentHubApp.swift
+//  AIAgentHub
+//
+//  Created by peerapat atawatana on 19/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct AIAgentHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIAgentHubTests/AIAgentHubTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIAgentHubTests.swift
+//  AIAgentHubTests
+//
+//  Created by peerapat atawatana on 19/4/2568 BE.
+//
+
+import Testing
+
+struct AIAgentHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
